27948abac322acb20022d2385b659c55eeb27647
Waleed Abdulla <waleed@ninua.com>
2016-11-19 18:32:13 -0800

    Workaround for missing serversecrets.py

--- fifteen_minute_difference/settings.py
+++ fifteen_minute_difference/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import serversecrets
-
-SMTP_LOGIN = serversecrets.SMTP_LOGIN
-SMTP_PASSWORD = serversecrets.SMTP_PASSWORD
+try:
+    import serversecrets
+    SMTP_LOGIN = serversecrets.SMTP_LOGIN
+    SMTP_PASSWORD = serversecrets.SMTP_PASSWORD
+except:
+    SMTP_LOGIN = ""
+    SMTP_PASSWORD = ""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
